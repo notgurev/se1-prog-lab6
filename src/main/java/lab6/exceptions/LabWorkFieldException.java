206988c8f39224b6d04d341c8b09b20152951050
notgurev <notgurev@gmail.com>
2020-04-11 03:29:21 +0300

    Классы исключений (из 5 лабы)

---
+++ src/main/java/lab6/exceptions/LabWorkFieldException.java
@@ -0,0 +1,7 @@
+package lab6.exceptions;
+
+/**
+ * Исключение для случаев, когда передаваемое поле не соответствует типу данных поля LabWork
+ */
+public class LabWorkFieldException extends RuntimeException {
+}
