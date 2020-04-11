4facfb0e2fdae1da7dd46acb71149dd2e90830a5
notgurev <notgurev@gmail.com>
2020-04-11 08:00:40 +0300

    Бинд.

--- src/main/java/lab6/server/ServerModule.java
+++ src/main/java/lab6/server/ServerModule.java
@@ -2,11 +2,13 @@ package lab6.server;
 
 import com.google.inject.AbstractModule;
 import lab6.server.interfaces.Server;
+import lab6.server.interfaces.ServerCommandReceiver;
 
 public class ServerModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(Server.class).to(ServerApp.class);
+        bind(ServerCommandReceiver.class).to(ServerCommandReceiverImpl.class);
     }
 
 }
