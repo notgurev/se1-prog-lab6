a02cd5bafeabd346ead7e76e1fff3e1fd43979ba
notgurev <notgurev@gmail.com>
2020-04-11 09:27:37 +0300

    @Singleton ServerIO

--- src/main/java/lab6/client/MyServerIO.java
+++ src/main/java/lab6/client/MyServerIO.java
@@ -1,8 +1,10 @@
 package lab6.client;
 
+import com.google.inject.Singleton;
 import lab6.client.commands.Command;
 import lab6.client.interfaces.ServerIO;
 
+@Singleton
 public class MyServerIO implements ServerIO {
     @Override
     public void sendToServer(Command command) {
