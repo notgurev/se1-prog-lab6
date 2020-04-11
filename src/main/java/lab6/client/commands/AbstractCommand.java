e51d07c43db6b7295698e92b48ef72ad848eaba9
notgurev <notgurev@gmail.com>
2020-04-11 06:28:13 +0300

    Переименовал isScriptCalling -> scriptCalling.

--- src/main/java/lab6/client/commands/AbstractCommand.java
+++ src/main/java/lab6/client/commands/AbstractCommand.java
@@ -4,13 +4,13 @@ package lab6.client.commands;
  * Базовый класс команды
  */
 public abstract class AbstractCommand implements Command {
-    private final boolean isScriptCalling;
+    private final boolean scriptCalling;
     private final boolean serverSide;
     private final String helpText;
     private final String key;
 
     public AbstractCommand(boolean isScriptCalling, boolean serverSide, String helpText, String key) {
-        this.isScriptCalling = isScriptCalling;
+        this.scriptCalling = isScriptCalling;
         this.serverSide = serverSide;
         this.helpText = helpText;
         this.key = key;
